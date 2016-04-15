d634792b55be0a3ed90552298e53e1b774c66abf
Pendragon16 <lbonnell@infspec.com>
2016-04-15 08:53:56 -0700

    april 15 number gen

--- CandyDefense/Assets/Scripts/LevelControl/mathGen.cs
+++ CandyDefense/Assets/Scripts/LevelControl/mathGen.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class mathGen : MonoBehaviour {
+public class mathGen : MonoBehaviour 
+{
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		//Random number between 1 and 100
+		AddNum = Mathf.RoundToInt (Random.value * (100 - 1 + 1)) + 1;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
 	}
 }
