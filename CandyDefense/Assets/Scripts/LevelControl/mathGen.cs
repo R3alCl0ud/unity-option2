466758dc072a78f1ed69e4f81d72f2b787e99736
Pendragon16 <lbonnell@infspec.com>
2016-04-27 07:46:14 -0700

    Math Functions
    Not sure if it will work properly, but its an outline

--- CandyDefense/Assets/Scripts/LevelControl/mathGen.cs
+++ CandyDefense/Assets/Scripts/LevelControl/mathGen.cs
@@ -3,24 +3,102 @@ using System.Collections;
 
 public class mathGen : MonoBehaviour 
 {
+	int a;
+	int AddNum ();
+	int sym ();
 
 	// Use this for initialization
 	void Start () 
 	{
-		//Random number between 1 and 100
+		//Random number between 1 and 20
+		int AddNum1 = AddNum ();
+		int AddNum2 = AddNum ();
+
 		if (easy)
 		{
-			int AddNum = Mathf.RoundToInt (Random.value * (12 - 1 + 1)) + 1;
+			a = 20;
+			sym ();
+
+			switch sym
+			{
+				case 1:
+				AddNum1 + AddNum2;
+				break;
+
+				case 2:
+				AddNum1 - AddNum2;
+				break;
+
+				default:
+				sym();
+				break;
+			}
+
+
+
+
+
 		} 
 		else if (medium)
 		{
-			int AddNum = Mathf.RoundToInt (Random.value * (12 - 1 + 1)) + 1;
+			a = 12;
+			sym ();
+
+			switch sym
+			{
+			case 1:
+				AddNum1 + AddNum2;
+				break;
+
+			case 2:
+				AddNum1 - AddNum2;
+				break;
+
+			case 3:
+				AddNum1 * AddNum2;
+				break;
+
+			case 4:
+				AddNum1 / AddNum2;
+				break;
+			}
+
 		} 
 		else if (hard)
 		{
-			int AddNum = Mathf.RoundToInt (Random.value * (12 - 1 + 1)) + 1;
+			a = 20;
+			sym ();
+
+			switch sym
+			{
+			case 1:
+				AddNum1 + AddNum2;
+				break;
+
+			case 2:
+				AddNum1 - AddNum2;
+				break;
+
+			case 3:
+				AddNum1 * AddNum2;
+				break;
+
+			case 4:
+				AddNum1 / AddNum2;
+				break;
+			}
 		}
 
+	}
+
+	int AddNum() {
+		Mathf.RoundToInt (Random.value * (a - 1 + 1)) + 1;
+		return a;
+	}
+
+	int sym() {
+		Mathf.RoundToInt (Random.value * (4 - 1 + 1)) + 1;
+		return sym;
 	}
 	
 	// Update is called once per frame
